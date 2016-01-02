0b122e8f61b9d7d86747a58f7faf0a103c0ede3d
Jon Volkmar <jonathonvolkmar@gmail.com>
2016-01-01 21:47:57 -0800

    add delay param to makegif

--- makegif.py
+++ makegif.py
@@ -8,7 +8,9 @@ if len(sys.argv) < 2:
     printUsage()
     exit(0)
 
-
+delay = 10
+if len(sys.argv) > 2:
+    delay = int(sys.argv[2])
     
 imageName = sys.argv[1]
 imagePath = path.join('output', imageName)
@@ -16,6 +18,6 @@ imageFiles = [path.join(imagePath, f) for f in listdir(imagePath) if f.endswith(
 imageFiles += reversed(imageFiles)
 
 outputFile = path.join('gifs', imageName + '.gif')
-command = 'convert ' + ' '.join(imageFiles) + ' ' + outputFile
+command = 'convert -delay ' + str(delay) + ' ' + ' '.join(imageFiles) + ' ' + outputFile
 print(command)
 system(command)
